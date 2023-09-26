588ef5dff9373ec9f9856e6bd5f6513f9d05585d
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-09-26 18:15:31 -0300

    changed colors

--- my-portifolio/src/components/layout/Header.tsx
+++ my-portifolio/src/components/layout/Header.tsx
@@ -1,16 +1,33 @@
 import React from "react";
 
+// images
+
+import profile from "../../assets/images/profileFelipe.jpg";
+
 type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <header className="bg-black text-white">
-      <h1 className="text-center font-bold">&lt;&gt; felipethome. &lt;/&gt;</h1>
-      <nav className="">
-        <ul className="flex gap-4 justify-center uppercase font-thin">
-          <li className="hover:cursor-pointer hover:text-[#49B8AC]"><a href="#home">Home</a></li>
-          <li className="hover:cursor-pointer hover:text-[#49B8AC]"><a href="#about">Sobre</a></li>
-          <li className="hover:cursor-pointer hover:text-[#49B8AC]">Projetos</li>
+    <header className="fixed z-10 w-full text-[#000000] px-12 py-6 bg-white space-y-4 lg:flex lg:justify-between">
+      <div className="flex justify-center gap-4 items-center">
+        <img
+          src={profile}
+          className="w-12 h-12 rounded-full border-black border p-0.5"
+          alt="Headshot of Felipe Thomé"
+        />
+        <h1 className="text-center font-bold uppercase">felipe thome.</h1>
+      </div>
+      <nav>
+        <ul className="flex gap-8 justify-center uppercase font-bold">
+          <li className="hover:cursor-pointer hover:text-[#49B8AC]">
+            <a href="#home">Home</a>
+          </li>
+          <li className="hover:cursor-pointer hover:text-[#49B8AC]">
+            <a href="#about">Sobre</a>
+          </li>
+          <li className="hover:cursor-pointer hover:text-[#49B8AC]">
+            Projetos
+          </li>
         </ul>
       </nav>
     </header>
--- my-portifolio/src/components/pages/Main.tsx
+++ my-portifolio/src/components/pages/Main.tsx
@@ -4,32 +4,101 @@ import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 type Props = {};
 
 const Main = (props: Props) => {
+  const skills: string[] = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "Tailwind",
+    "Git",
+    "Github",
+    "Responsive Design",
+  ];
+
   return (
-    <main className="bg-black text-white p-8 space-y-4">
-      <h1 id="home">
-        <span className="text-[#49B8AC]">Olá, meu nome é</span>
-        <br /> Felipe Thomé. <br /> Desenvolvo aplicações web.
-      </h1>
-      <p>Dev Front End</p>
-      <div className="text-2xl flex gap-4">
-        <a href="https://github.com/felipetn1989" target="blank">
-          <FaGithub className="hover:cursor-pointer hover:text-[#49B8AC]" />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/felipe-thome-a0094a276/"
-          target="blank"
-        >
-          <FaLinkedin className="hover:cursor-pointer hover:text-[#49B8AC]" />
-        </a>
-        <a href="https://instagram.com/ftn1989" target="blank">
-          <FaInstagram className="hover:cursor-pointer hover:text-[#49B8AC]" />
-        </a>
+    <main className="bg-[#0f1c3a] text-[#d8d8d8] px-8 pt-24 pb-4 lg:px-16 space-y-8 max-w-[80rem] m-auto">
+      <div className="space-y-8 py-16">
+        <div className="space-y-10">
+          <h1 id="home" className="text-[4rem] font-bold leading-[4.5rem]">
+            <span className="text-[#4DBFC4] text-base font-bold">
+              Olá, meu nome é
+            </span>
+            <br /> Felipe Thomé. <br />{" "}
+            <span className="text-[#777777]">Desenvolvo aplicações web.</span>
+            <p className="text-base pt-4">Dev Front End</p>
+          </h1>
+
+          <button className="bg-[#49B8AC] text-white py-4 px-6 font-bold uppercase rounded-xl hover:bg-white hover:text-[#49B8AC]">
+            Meus Projetos
+          </button>
+        </div>
+        <div className="text-2xl flex gap-4 pt-8">
+          <a href="https://github.com/felipetn1989" target="blank">
+            <FaGithub className="hover:cursor-pointer hover:text-[#49B8AC]" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/felipe-thome-a0094a276/"
+            target="blank"
+          >
+            <FaLinkedin className="hover:cursor-pointer hover:text-[#49B8AC]" />
+          </a>
+          <a href="https://instagram.com/ftn1989" target="blank">
+            <FaInstagram className="hover:cursor-pointer hover:text-[#49B8AC]" />
+          </a>
+        </div>
       </div>
-      <div id="about">
-        <h2>01. Sobre</h2>
-        <p>Recentemente resolvi me desafiar no mundo da programação para aprender novas habilidades e desde então, descoberto esse novo interesse, passei a desenvolver aplicações web baseadas em JavaScript, focando em design responsivo e mobile-first. Meus projetos atualmente envolvem o uso de frameworks como Tailwind e React, além de CSS e JS puros. Desenvolvi aplicações a partir de APIs que são dinamicamente incorporadas na página, buscando criar uma experiência visualmente engajante para o usuário.</p>
-        <p>Antes de iniciar meus estudos em desenvolvimento de software, obtive uma graduação em engenharia mecânica, certificação de piloto privado de aviões, certificação para atuação em distribuição de produtos financeiros e atuei primariamente como professor particular de ciências exatas e inglês.</p>
-        <p>Busco uma oportunidade para desenvolver projetos maiores em equipe, contribuindo com o conhecimento que já tenho e aprendendo ainda mais sobre a área.</p>
+      <div id="about" className="lg:grid lg:grid-cols-2 lg:gap-32">
+        <div className="space-y-4">
+          <div className="w-fit">
+            <h2 className="text-[#49B8AC] uppercase font-thin text-2xl">
+              <span className="text-white">01.</span> Sobre
+            </h2>
+            <div className="bg-[#49B8AC] w-full h-1 skew-x-[-30deg]"></div>
+          </div>
+          <div className="space-y-4 text-justify text-[#808080]">
+            <p>
+              Recentemente resolvi me desafiar no mundo da programação para
+              aprender novas habilidades e desde então, descoberto esse novo
+              interesse, passei a desenvolver aplicações web baseadas em
+              JavaScript, focando em design responsivo e mobile-first. Meus
+              projetos atualmente envolvem o uso de frameworks como Tailwind e
+              React, além de CSS e JS puros. Desenvolvi aplicações a partir de
+              APIs que são dinamicamente incorporadas na página, buscando criar
+              uma experiência visualmente engajante para o usuário.
+            </p>
+            <p>
+              Antes de iniciar meus estudos em desenvolvimento de software,
+              obtive uma graduação em engenharia mecânica, certificação de
+              piloto privado de aviões, certificação para atuação em
+              distribuição de produtos financeiros e atuei primariamente como
+              professor particular de ciências exatas e inglês.
+            </p>
+            <p>
+              Busco uma oportunidade para desenvolver projetos maiores em
+              equipe, contribuindo com o conhecimento que já tenho e aprendendo
+              ainda mais sobre a área.
+            </p>
+          </div>
+        </div>
+        <div className="space-y-2">
+          <h2 className="font-thin text-2xl uppercase">Habilidades</h2>
+          <div className="flex gap-6 flex-wrap w-80 text-white">
+            {skills.map((element, index) => (
+              <span
+                key={index}
+                className="bg-[#49B8AC] w-fit px-4 py-2 rounded-md"
+              >
+                {element}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="w-fit">
+        <h2 className="text-[#49B8AC] uppercase font-thin text-2xl">
+          <span className="text-white">02.</span> Projetos
+        </h2>
+        <div className="bg-[#4DBFC4] w-full h-1 skew-x-[-30deg]"></div>
       </div>
     </main>
   );
